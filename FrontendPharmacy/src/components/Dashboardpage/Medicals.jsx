101531b44726357dbc40cd137c36b95b73c75606
tchami <131756888+tchamianest@users.noreply.github.com>
2024-06-10 15:37:58 +0200

    Add the sectiion for admin to controll their stock

--- FrontendPharmacy/src/components/Dashboardpage/Medicals.jsx
+++ FrontendPharmacy/src/components/Dashboardpage/Medicals.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Medicals = () => {
+  const navigate = useNavigate();
+  const [medical, setMedical] = useState([]);
   const users = JSON.parse(localStorage.getItem("user"));
   const Logout = () => {
     localStorage.clear();
     navigate("/");
   };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get(
+          "http://localhost:5000/api/product/medicals",
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              Authorization: localStorage.getItem("HeaderToken"),
+            },
+          }
+        );
+        setMedical(result.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="">
       <div className="flex justify-between pr-20 p-3">
@@ -33,162 +57,30 @@ const Medicals = () => {
       </div>
       <div className="w-[100%] flex justify-center mt-10">
         <div className="w-[80%]  h-[80%] border-[1px] overflow-auto py-10 px-5 max-h-[65vh]">
-          <div className="flex justify-between w-[100%] items-center border-b p-2">
-            <p className="font-semibold ">Medical Name</p>
-            <p className=""> 3000 rwf</p>
-            <p className="">Items : 10 </p>
-            <div className="flex gap-5 justify-end">
-              <button className="bg-blue-600 px-10 p-1 rounded-sm text-white hover:bg-blue-700">
-                Update{" "}
-              </button>
-              <button className="bg-red-600 px-10 p-1 rounded-sm text-white hover:bg-red-700">
-                Delete
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between w-[100%] items-center border-b p-2">
-            <p className="font-semibold ">Medical Name</p>
-            <p className=""> 3000 rwf</p>
-            <p className="">Items : 10 </p>
-            <div className="flex gap-5 justify-end">
-              <button className="bg-blue-600 px-10 p-1 rounded-sm text-white hover:bg-blue-700">
-                Update{" "}
-              </button>
-              <button className="bg-red-600 px-10 p-1 rounded-sm text-white hover:bg-red-700">
-                Delete
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between w-[100%] items-center border-b p-2">
-            <p className="font-semibold ">Medical Name</p>
-            <p className=""> 3000 rwf</p>
-            <p className="">Items : 10 </p>
-            <div className="flex gap-5 justify-end">
-              <button className="bg-blue-600 px-10 p-1 rounded-sm text-white hover:bg-blue-700">
-                Update{" "}
-              </button>
-              <button className="bg-red-600 px-10 p-1 rounded-sm text-white hover:bg-red-700">
-                Delete
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between w-[100%] items-center border-b p-2">
-            <p className="font-semibold ">Medical Name</p>
-            <p className=""> 3000 rwf</p>
-            <p className="">Items : 10 </p>
-            <div className="flex gap-5 justify-end">
-              <button className="bg-blue-600 px-10 p-1 rounded-sm text-white hover:bg-blue-700">
-                Update{" "}
-              </button>
-              <button className="bg-red-600 px-10 p-1 rounded-sm text-white hover:bg-red-700">
-                Delete
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between w-[100%] items-center border-b p-2">
-            <p className="font-semibold ">Medical Name</p>
-            <p className=""> 3000 rwf</p>
-            <p className="">Items : 10 </p>
-            <div className="flex gap-5 justify-end">
-              <button className="bg-blue-600 px-10 p-1 rounded-sm text-white hover:bg-blue-700">
-                Update{" "}
-              </button>
-              <button className="bg-red-600 px-10 p-1 rounded-sm text-white hover:bg-red-700">
-                Delete
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between w-[100%] items-center border-b p-2">
-            <p className="font-semibold ">Medical Name</p>
-            <p className=""> 3000 rwf</p>
-            <p className="">Items : 10 </p>
-            <div className="flex gap-5 justify-end">
-              <button className="bg-blue-600 px-10 p-1 rounded-sm text-white hover:bg-blue-700">
-                Update{" "}
-              </button>
-              <button className="bg-red-600 px-10 p-1 rounded-sm text-white hover:bg-red-700">
-                Delete
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between w-[100%] items-center border-b p-2">
-            <p className="font-semibold ">Medical Name</p>
-            <p className=""> 3000 rwf</p>
-            <p className="">Items : 10 </p>
-            <div className="flex gap-5 justify-end">
-              <button className="bg-blue-600 px-10 p-1 rounded-sm text-white hover:bg-blue-700">
-                Update{" "}
-              </button>
-              <button className="bg-red-600 px-10 p-1 rounded-sm text-white hover:bg-red-700">
-                Delete
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between w-[100%] items-center border-b p-2">
-            <p className="font-semibold ">Medical Name</p>
-            <p className=""> 3000 rwf</p>
-            <p className="">Items : 10 </p>
-            <div className="flex gap-5 justify-end">
-              <button className="bg-blue-600 px-10 p-1 rounded-sm text-white hover:bg-blue-700">
-                Update{" "}
-              </button>
-              <button className="bg-red-600 px-10 p-1 rounded-sm text-white hover:bg-red-700">
-                Delete
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between w-[100%] items-center border-b p-2">
-            <p className="font-semibold ">Medical Name</p>
-            <p className=""> 3000 rwf</p>
-            <p className="">Items : 10 </p>
-            <div className="flex gap-5 justify-end">
-              <button className="bg-blue-600 px-10 p-1 rounded-sm text-white hover:bg-blue-700">
-                Update{" "}
-              </button>
-              <button className="bg-red-600 px-10 p-1 rounded-sm text-white hover:bg-red-700">
-                Delete
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between w-[100%] items-center border-b p-2">
-            <p className="font-semibold ">Medical Name</p>
-            <p className=""> 3000 rwf</p>
-            <p className="">Items : 10 </p>
-            <div className="flex gap-5 justify-end">
-              <button className="bg-blue-600 px-10 p-1 rounded-sm text-white hover:bg-blue-700">
-                Update{" "}
-              </button>
-              <button className="bg-red-600 px-10 p-1 rounded-sm text-white hover:bg-red-700">
-                Delete
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between w-[100%] items-center border-b p-2">
-            <p className="font-semibold ">Medical Name</p>
-            <p className=""> 3000 rwf</p>
-            <p className="">Items : 10 </p>
-            <div className="flex gap-5 justify-end">
-              <button className="bg-blue-600 px-10 p-1 rounded-sm text-white hover:bg-blue-700">
-                Update{" "}
-              </button>
-              <button className="bg-red-600 px-10 p-1 rounded-sm text-white hover:bg-red-700">
-                Delete
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-between w-[100%] items-center border-b p-2">
-            <p className="font-semibold ">Medical Name</p>
-            <p className=""> 3000 rwf</p>
-            <p className="">Items : 10 </p>
-            <div className="flex gap-5 justify-end">
-              <button className="bg-blue-600 px-10 p-1 rounded-sm text-white hover:bg-blue-700">
-                Update{" "}
-              </button>
-              <button className="bg-red-600 px-10 p-1 rounded-sm text-white hover:bg-red-700">
-                Delete
-              </button>
-            </div>
-          </div>
+          {medical &&
+            medical.map((el) => (
+              <>
+                <div className="flex justify-between w-[100%] items-center border-b p-2">
+                  <div className="w-[20%]">
+                    <p className="font-semibold ">{el.productName}</p>
+                  </div>
+                  <div className="w-[20%]">
+                    <p className=""> {el.productPrice} rwf</p>
+                  </div>
+                  <p className="">
+                    {el.isAvailable ? "Available" : "Un Available"}
+                  </p>
+                  <div className="flex gap-5 justify-end">
+                    <button className="bg-blue-600 px-10 p-1 rounded-sm text-white hover:bg-blue-700">
+                      Update{" "}
+                    </button>
+                    <button className="bg-red-600 px-10 p-1 rounded-sm text-white hover:bg-red-700">
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </>
+            ))}
         </div>
       </div>
     </div>
