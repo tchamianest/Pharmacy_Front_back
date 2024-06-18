24ebf2051f10d807979fc56a927b405c4f2d6e11
tchami <131756888+tchamianest@users.noreply.github.com>
2024-06-18 20:06:10 +0200

    The change on the Search page and change The UI

--- FrontendPharmacy/src/components/Page/SearchPage.jsx
+++ FrontendPharmacy/src/components/Page/SearchPage.jsx
@@ -2,43 +2,27 @@ import React from "react";
 import Footer from "../Footer/Footer";
 import Navbar from "../navbar/Navbar";
 import Product from "../cardFront/Product";
+import Cardseacrh from "../cardFront/Cardseacrh";
 
 function SearchPage() {
   return (
     <div>
-      <div className="flex justify-center dark:bg-gray-800 items-center flex-col  gap-8">
+      <div className="flex max-h-[100] fixed justify-center dark:bg-gray-800 items-center flex-col  gap-8">
         <Navbar />
       </div>
-      <div className="p-10 bg-gray-800 flex gap-4">
+      <div className="p-10  bg-gray-800 flex gap-4   content-center">
         {/* left sections  */}
-        <div className="w-[20%] bg-gray-300 rounded-sm">d</div>
+        <div className="w-[20%] mt-[90px] h-[100vh]   bg-gray-300 rounded-sm">
+          d
+        </div>
         {/* the search section  */}
-        <div className="w-[80%] grid-cols-4 grid">
-          <Product
-            SellerName="Kalisa daniel"
-            location="Kigalida"
-            productName="Shoes"
-          />
-          <Product
-            SellerName="Kalisa daniel"
-            location="Kigalida"
-            productName="Shoes"
-          />
-          <Product
-            SellerName="Kalisa daniel"
-            location="Kigalida"
-            productName="Shoes"
-          />
-          <Product
-            SellerName="Kalisa daniel"
-            location="Kigalida"
-            productName="Shoes"
-          />
-          <Product
-            SellerName="Kalisa daniel"
-            location="Kigalida"
-            productName="Shoes"
-          />
+        <div className="w-[80%] mt-[90px]  grid-cols-1 grid gap-4">
+          <Cardseacrh />
+          <Cardseacrh />
+          <Cardseacrh />
+          <Cardseacrh />
+          <Cardseacrh />
+          <Cardseacrh />
         </div>
       </div>
       <Footer />
