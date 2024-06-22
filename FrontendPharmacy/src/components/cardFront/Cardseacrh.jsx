904f7a57aad54079caf885a4bfe7a367b4fc5450
tchami <131756888+tchamianest@users.noreply.github.com>
2024-06-22 14:05:35 +0200

    Feat(Search ): Implements the Search with The near Location

--- FrontendPharmacy/src/components/cardFront/Cardseacrh.jsx
+++ FrontendPharmacy/src/components/cardFront/Cardseacrh.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import store from "../../assets/hero/store.png";
 
-function Cardseacrh() {
+function Cardseacrh(props) {
   return (
-    <div className="w-[80%] shadow-md bg-gray-200 p-5 rounded-sm ">
+    <div className="w-[80%] shadow-md max-h-[300px] bg-gray-200 p-5 rounded-sm ">
       <div className="flex gap-5">
         <div className="w-[20%]">
-          <img src={store} alt="image" />
+          <img src={props.image} alt="image" />
         </div>
         <div className="flex-1 ">
-          <p className="font-bold text-2xl">Product Title</p>
-          <p className="text-xs">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
-            deleniti omnis ab, commodi, dicta eveniet culpa, perspiciatis
-            ratione eligendi delectus facere quisquam possimus soluta dolorum at
-            nihil architecto ipsum option.
-          </p>
+          <p className="font-bold text-2xl mb-3">{props.title}</p>
+          <p className="text-xs">{props.description}</p>
           <div className="flex mt-5 gap-3">
-            <p className="font-bold text-primary">Location :</p>
-            <p>Kamonyi Rwanda South</p>
+            {props.location && (
+              <p className="font-bold text-primary">Location :</p>
+            )}
+            <p>{props.location}</p>
           </div>
-          <div className="flex justify-end mt-5 gap-3 left">
-            <button className="p-3 text-right bg-primary/95 rounded-full text-white hover:bg-secondary">
-              View Location
-            </button>
-          </div>
+
+          {props.location && (
+            <div className="flex justify-end mt-5 gap-3 left">
+              <button className="p-3 text-right bg-primary/95 rounded-full text-white hover:bg-secondary">
+                View Location
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
